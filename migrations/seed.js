d2f21de7bfbb27756b0f88cb0d278f92c8f7a4ab
Daniel Martinez <dmartz86@gmail.com>
2015-08-12 13:45:13 -0500

    Add migration example

---
+++ migrations/seed.js
@@ -0,0 +1,20 @@
+var db = require('../../../helpers/models');
+var foos = require('./data/foos.json');
+var bars = require('./data/bars.json');
+
+var persist = function(list, model) {
+  for (var r in list){
+    if(list.hasOwnProperty(r)){
+      model.Insert(list[r]);
+    }
+  }
+};
+
+persist(roles, db.foos);
+persist(users, db.bars);
+
+// add menu
+db.settings.Update(
+  {"type" : "properties"},
+  { $push: { data.user: "bars"}, data.admin: "bars"} }
+);
